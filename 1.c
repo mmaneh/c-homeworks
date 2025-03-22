26b376bbd592a4668219f17d90887f112949451a
mmaneh <mmanemkhitaryan@gmail.com>
2025-03-23 01:21:55 +0400

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
      int main () {
-         int n=11;
-     while(n-- >0){
+         int n=10;
+     while(n>=0){
          printf("%d\n", n);
+          n--;
      }
          int a=0;
-     while(a++ < 10){
+     while(a < 10){
          printf("reversed order: %d\n", a);
-     
+     a++;
      }
      return 0;
      
